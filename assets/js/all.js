ca85875fed5499f00970a6189466eab6a9bfcc23
Amber <melody19941231@gmail.com>
2022-08-19 16:17:17 +0800

    Update 2022-08-19T08:17:14.114Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,4 +3,24 @@
 $(function () {
   console.log('Hello Bootstrap5');
 });
+"use strict";
+
+var swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  effect: "fade",
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets'
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  controller: {
+    inverse: true
+  }
+});
 //# sourceMappingURL=all.js.map
